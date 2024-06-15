750838cd5062a1585db93dfbbe8ae9be2a6a3fc0
Jamie <jamie.gillett@gmail.com>
2024-06-15 18:58:20 +0100

    split out different data types

--- TexturePacker.py
+++ TexturePacker.py
@@ -8,6 +8,7 @@ class Packer:
         self.verbose = verbose
 
     def load_images(self, image_paths):
+        if self.verbose: print("load_images")
         image_arrays = []
         for image_path in image_paths:
             image = Image.open(image_path)
@@ -20,77 +21,96 @@ class Packer:
         return image_arrays
 
     def identify_bounding_boxes(self, image_arrays):
+        if self.verbose: print("identify_bounding_boxes")
         bounding_boxes = []
         for i, image_array in enumerate(image_arrays):
             # Check if the image is fully opaque
             if np.all(image_array[:, :, 3] == 255):
                 # Treat the entire image as a single box
-                boxes = [((0, 0, image_array.shape[1], image_array.shape[0]),i)]  # x-offset, y-offset, width, height
+                # boxes = [((0, 0, image_array.shape[1], image_array.shape[0]),i)]  # ((x-offset, y-offset, width, height), image index)
+                boxes = [((0, 0), (image_array.shape[1], image_array.shape[0]),i)]  # ((x-offset, y-offset), (width, height), image index)
             else:
                 gray = cv2.cvtColor(image_array, cv2.COLOR_RGBA2GRAY)
                 _, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
                 contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                boxes = [((x,y,w,h),i) for x,y,w,h in [cv2.boundingRect(contour) for contour in contours]]
+                # boxes = [((x,y,w,h),i) for x,y,w,h in [cv2.boundingRect(contour) for contour in contours]]
+                boxes = [((x,y),(w,h),i) for x,y,w,h in [cv2.boundingRect(contour) for contour in contours]]
                 if not boxes:
-                    boxes = [((0, 0, image_array.shape[1], image_array.shape[0]),i)] # Ensure at least one bounding box
+                    # boxes = [((0, 0, image_array.shape[1], image_array.shape[0]),i)] # Ensure at least one bounding box
+                    boxes = [((0, 0), (image_array.shape[1], image_array.shape[0]),i)] # Ensure at least one bounding box
             bounding_boxes.extend(boxes)
-        bounding_boxes = sorted(bounding_boxes, key=lambda x: x[0][3], reverse=True) # Sort bounding_boxes by height (largest to smallest)
-        return bounding_boxes
+        # bounding_boxes = sorted(bounding_boxes, key=lambda x: x[0][3], reverse=True) # Sort bounding_boxes by height (largest to smallest)
+        bounding_boxes = sorted(bounding_boxes, key=lambda x: x[1][1], reverse=True) # Sort bounding_boxes by height (largest to smallest)
+        coordinates, dimensions, image_indices = [_ for _ in list(zip(*bounding_boxes))]
+        return coordinates, dimensions, image_indices
+    
+    def calculate_packing_bounds(self, images):
+        if self.verbose: print("calculate_packing_bounds")
+        width = max(image.shape[1] for image in images)
+        height = sum(image.shape[0] for image in images)
+        return width, height
 
-    def pack(self, all_images, bounding_boxes):
-        packed_positions = []
-        # Initialize free rectangles with one big rectangle
-        # TODO possible extract new image size function and pass in size as an argument
-        max_width = max(image.shape[1] for image in all_images)
-        max_height = sum(image.shape[0] for image in all_images) # If the height is based on the images and not the boxes, can I break this?
-        empty_areas = [(0, 0, max_width, max_height)]
-        for box, image_index in bounding_boxes:
-            w, h = box[2], box[3]
+    def pack(self, bounds, boxes):
+        if self.verbose: print("pack")
+        new_coordinates = []
+        max_width, max_height = bounds
+        empty_areas = [((0, 0), (max_width, max_height))]
+        for box in boxes:
+            box_width, box_height = box[0], box[1]
             best_empty_area = None
             best_empty_area_index = -1
             best_fit = None
             # Find the best fitting free rectangle
             for i, empty_area in enumerate(empty_areas):
-                fw, fh = empty_area[2], empty_area[3]
-                if fw >= w and fh >= h:
-                    fit = (fw - w) * (fh - h)
+                test_width, test_height = empty_area[1][0], empty_area[1][1]
+                if test_width >= box_width and test_height >= box_height:
+                    fit = (test_width - box_width) * (test_height - box_height)
                     if best_fit is None or fit < best_fit:
                         best_fit = fit
                         best_empty_area = empty_area
                         best_empty_area_index = i
             if best_empty_area is None:
-                # how would you get here?!
                 continue
             # Place the rectangle in the best fitting free rectangle
-            fx, fy, fw, fh = best_empty_area
-            packed_positions.append((fx, fy, w, h, box[0], box[1], image_index))
+            new_coordiante = empty_area[0]
+            new_coordinates.append(new_coordiante)
             # Split the free rectangle
-            new_free_rects = []
-            new_free_rects.append((fx + w, fy, fw - w, h))  # Right part
-            new_free_rects.append((fx, fy + h, fw, fh - h))  # Bottom part
+            empty_area_splits = []
+            new_x, new_y = new_coordiante
+            empty_area_splits.append(((new_x + box_width, new_y), (test_width - box_width, box_height)))  # Right part
+            empty_area_splits.append(((new_x, new_y + box_height), (test_width, test_height - box_height)))  # Bottom part
             # Replace the used free rectangle
             empty_areas.pop(best_empty_area_index)
-            for new_free_rect in new_free_rects:
-                if new_free_rect[2] > 0 and new_free_rect[3] > 0:
-                    empty_areas.append(new_free_rect)
-        return packed_positions
+            for area_split in empty_area_splits:
+                if area_split[1][0] > 0 and area_split[1][1] > 0:
+                    empty_areas.append(area_split)
+        return new_coordinates
 
-    def generate_image(self, all_images, packed_positions):
-        # Create a new packed image with the calculated size
-        max_width = max(px + pw for px, py, pw, ph, ox, oy, img_idx in packed_positions)
-        max_height = max(py + ph for px, py, pw, ph, ox, oy, img_idx in packed_positions)
-        packed_image_np = np.zeros((max_height, max_width, 4), dtype=np.uint8)
-        # Place rectangles into the packed image
-        for px, py, pw, ph, ox, oy, img_idx in packed_positions:
-            packed_image_np[py:py+ph, px:px+pw] = all_images[img_idx][oy:oy+ph, ox:ox+pw]
-        packed_image = Image.fromarray(packed_image_np) # Convert back to Image
-        return packed_image
+    def generate_image(self, images, original_coordinates, texture_dimensions, new_coordinates, image_indices):
+        if self.verbose: print("generate_image")
+        n = len(original_coordinates)
+        # Calculuate the new image size
+        max_width = max(new_coordinates[i][0]+texture_dimensions[i][0] for i in range(n))
+        max_height = max(new_coordinates[i][1]+texture_dimensions[i][1] for i in range(n))
+        new_image_array = np.zeros((max_height, max_width, 4), dtype=np.uint8) # Create a new image array with the calculated size
+        # Place textures into the new image
+        for i in range(n):
+            old_x, old_y = original_coordinates[i]
+            width, height = texture_dimensions[i]
+            new_x, new_y = new_coordinates[i]
+            image_id = image_indices[i]
+            new_image_array[new_y:new_y+height, new_x:new_x+width] = images[image_id][old_y:old_y+height, old_x:old_x+width]
+        # for px, py, pw, ph, ox, oy, img_idx in packed_positions:
+            # new_image_array[py:py+ph, px:px+pw] = images[img_idx][oy:oy+ph, ox:ox+pw]
+        new_image = Image.fromarray(new_image_array) # Convert back to Image
+        return new_image
 
     def process_textures(self, image_paths):
-        all_images = self.load_images(image_paths)
-        bounding_boxes = self.identify_bounding_boxes(all_images)
-        packed_positions = self.pack(all_images, bounding_boxes)
-        output_image = self.generate_image(all_images, packed_positions)
+        images = self.load_images(image_paths)
+        original_coordinates, texture_dimensions, texture_image_indices = self.identify_bounding_boxes(images)
+        packing_bounds = self.calculate_packing_bounds(images)
+        new_coordinates = self.pack(packing_bounds, texture_dimensions)
+        output_image = self.generate_image(images, original_coordinates, texture_dimensions, new_coordinates, texture_image_indices)
         return output_image
 
 
